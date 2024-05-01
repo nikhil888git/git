d9bdb54989075ed36536609055b5f34e4276000a
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-05-01 17:54:22 +0530

    hiaaaa

---
+++ pract.js
@@ -0,0 +1,13 @@
+console.log("hello all");
+console.log("hello all");
+console.log("hello all");
+console.log("hello all");
+console.log("hello all");
+console.log("without stast bras=nch change after commit");
+console.log("without stash branch change after commit");
+console.log("with stash branch change after commit");
+console.log("with stash branch change after commit # 1 more time ---------");
+console.log("with stash branch change after commit #2 more time ---------");
+console.log("with stash branch change after commit #2 more time ---------");
+console.log("with stash branch change after commit #2 more time ---------");
+//1212112121212121app
