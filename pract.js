cc0bd85b6e9f34cd47e71c39f9ea7c4d8e37adb8
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-05-01 16:45:50 +0530

    this is cmit

--- pract.js
+++ pract.js
@@ -4,3 +4,4 @@ console.log("hello all");
 console.log("hello all");
 console.log("hello all");
 console.log("without stast bras=nch change after commit");
+console.log("without stash branch change after commit");
